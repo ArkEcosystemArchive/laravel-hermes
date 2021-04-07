c048e5f2a4b102869637a7a7fdf0926b29f9d6a8
Highjhacker <jolanbeer@gmail.com>
2021-04-07 17:49:33 +0200

    refactor: additional notification types

---
+++ src/Enums/NotificationTypeEnum.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace ARKEcosystem\Hermes\Enums;
+
+final class NotificationTypeEnum
+{
+    const SUCCESS = 'success';
+
+    const WARNING = 'warning';
+
+    const DANGER = 'danger';
+
+    const BLOCKED = 'blocked';
+
+    const COMMENT = 'comment';
+
+    const MENTION = 'mention';
+}
