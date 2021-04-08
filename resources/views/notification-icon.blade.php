cfe7e234c37a30cdfd5e4d8d3081ce5e7432ff9c
Highjhacker <jolanbeer@gmail.com>
2021-04-08 15:16:39 +0200

    refactor: requested changes

--- resources/views/notification-icon.blade.php
+++ resources/views/notification-icon.blade.php
@@ -34,23 +34,23 @@
                     </div>
                 @elseif ($type === ARKEcosystem\Hermes\Enums\NotificationFilterEnum::SUCCESS)
                     <div class="flex items-center justify-center flex-shrink-0 w-6 h-6 rounded-full bg-theme-success-100">
-                        <x-ark-icon name="notifications.success" size="xs" class="text-theme-success-600" />
+                        <x-ark-icon name="notifications.success" size="sm" class="text-theme-success-600" />
                     </div>
                 @elseif ($type === ARKEcosystem\Hermes\Enums\NotificationFilterEnum::WARNING)
                     <div class="flex items-center justify-center flex-shrink-0 w-6 h-6 rounded-full bg-theme-warning-100">
-                        <x-ark-icon name="notifications.warning" size="xs" class="text-theme-warning-600" />
+                        <x-ark-icon name="notifications.warning" size="sm" class="text-theme-warning-600" />
                     </div>
                 @elseif ($type === ARKEcosystem\Hermes\Enums\NotificationFilterEnum::BLOCKED)
                     <div class="flex items-center justify-center flex-shrink-0 w-6 h-6 rounded-full bg-theme-secondary-200">
-                        <x-ark-icon name="notifications.blocked" size="xs" class="text-theme-secondary-900" />
+                        <x-ark-icon name="notifications.blocked" size="sm" class="text-theme-secondary-900" />
                     </div>
                 @elseif ($type === ARKEcosystem\Hermes\Enums\NotificationFilterEnum::COMMENT)
                     <div class="flex items-center justify-center flex-shrink-0 w-6 h-6 rounded-full bg-theme-secondary-200">
-                        <x-ark-icon name="notifications.comment" size="xs" class="text-theme-secondary-900" />
+                        <x-ark-icon name="notifications.comment" size="sm" class="text-theme-secondary-900" />
                     </div>
                 @elseif ($type === ARKEcosystem\Hermes\Enums\NotificationFilterEnum::MENTION)
                     <div class="flex items-center justify-center flex-shrink-0 w-6 h-6 rounded-full bg-theme-secondary-200">
-                        <x-ark-icon name="notifications.mention" size="xs" class="text-theme-secondary-900" />
+                        <x-ark-icon name="notifications.mention" size="sm" class="text-theme-secondary-900" />
                     </div>
                 @endif
             </div>
