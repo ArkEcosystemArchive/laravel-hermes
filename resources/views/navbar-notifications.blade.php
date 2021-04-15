4db6d0cbab0cca94fee92fb274269965962cb5f7
Highjhacker <jolanbeer@gmail.com>
2021-04-15 11:15:46 +0200

    refactor: add loop index to dusk selector to avoid duplicates

--- resources/views/navbar-notifications.blade.php
+++ resources/views/navbar-notifications.blade.php
@@ -2,7 +2,7 @@
     @if(Auth::check() && $notificationCount > 0)
         <div class="inline-block w-full py-4 md:py-4" dusk="navigation-notifications">
             @foreach($currentUser->notifications->take(4) as $notification)
-                <div class="flex px-2 py-6 leading-5 {{ ! $loop->last ? 'border-b border-dashed border-theme-secondary-200' : '' }}" dusk="navigation-notification">
+                <div class="flex px-2 py-6 leading-5 {{ ! $loop->last ? 'border-b border-dashed border-theme-secondary-200' : '' }}" dusk="navigation-notification-{{$loop->index}}">
                     <x-hermes-notification-icon :notification="$notification" :type="$notification->data['type']" />
 
                     <div class="flex flex-col w-full ml-5 space-y-1 overflow-auto">
