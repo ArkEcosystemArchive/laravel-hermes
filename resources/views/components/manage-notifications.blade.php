7ba7c4cb162579bac849e56ecbff78f20ea3f020
ItsANameToo <35610748+ItsANameToo@users.noreply.github.com>
2021-04-16 14:20:33 +0200

    fix: notification dropdown button border radius (#47)

--- resources/views/components/manage-notifications.blade.php
+++ resources/views/components/manage-notifications.blade.php
@@ -40,7 +40,7 @@
                 </div>
 
                 <div class="w-10 sm:relative">
-                    <x-ark-dropdown wrapper-class="top-0 right-0 inline-block text-left sm:absolute" dropdown-classes="left-0 w-64 mt-3" button-class="flex justify-center w-10 h-10 rounded-xl bg-theme-primary-100 text-theme-primary-600">
+                    <x-ark-dropdown wrapper-class="top-0 right-0 inline-block text-left sm:absolute" dropdown-classes="left-0 w-64 mt-3" button-class="flex justify-center w-10 h-10 rounded bg-theme-primary-100 text-theme-primary-600">
                         <div class="py-3">
                             <div class="cursor-pointer dropdown-entry" wire:click="markSelectedAsRead">
                                 @lang('hermes::menus.notifications-dropdown.mark_selected_as_read')
