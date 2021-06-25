695489997bc82f923c26eaf2bdadf779cafb7572
ItsANameToo <itsanametoo@protonmail.com>
2021-06-25 11:51:47 +0200

    wip

--- resources/views/components/notifications-indicator.blade.php
+++ resources/views/components/notifications-indicator.blade.php
@@ -1,6 +1,6 @@
 <div class="group">
     @if($this->notificationsUnread ?? false)
-        <span class="absolute right-0 block p-1 mr-2 -mt-4 bg-white rounded-full group-hover:bg-theme-primary-100">
+        <span class="absolute right-0 block p-1 mr-2 -mt-4 bg-white rounded-full transition-default group-hover:bg-theme-primary-100">
             <span class="block w-1 h-1 rounded-full border-3 bg-theme-danger-500 border-theme-danger-500"></span>
         </span>
     @endif
